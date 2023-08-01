d1f907a9be0c1e00a8f1f163c9878f3f2f7dfbf4
GonzaloMartin13 <gonzalo.m.martin@gmail.com>
2023-07-31 21:38:27 -0300

    M1 HM01 hecha

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,20 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   let decimal = 0;
+   let array = num.split("");
+   let arrayInvertido = array.reverse();
 
-function DecimalABinario(num) {}
+   for (let i = 0; i < num.length; i++){
+      decimal = decimal + arrayInvertido[i] * 2 ** i;
+   }
+   return decimal;
+}
+function DecimalABinario(num) {
+   let binario = num.toString(2);
+   return binario
+}
+
 
 module.exports = {
    BinarioADecimal,
